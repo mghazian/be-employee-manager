e62bae1110b9c1c76f75e3dc4a3219d9695d0450
Muhammad Ghazian <mghazian@github.com>
2025-10-22 17:20:15 +0700

    Initial commit: DB schema + Container compose manifest initialization

---
+++ src/main/resources/db/migration/V20251022_145033__initial_schema.sql
@@ -0,0 +1,57 @@
+create table if not exists departments (
+    id bigserial primary key,
+    code varchar not null,
+    name varchar not null,
+
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp
+);
+
+create unique index if not exists idx_departments_code on departments (code);
+
+create table if not exists locations (
+    id bigserial primary key,
+    code varchar not null,
+    name varchar not null,
+    address varchar not null,
+
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp
+);
+
+create unique index if not exists idx_locations_code on locations (code);
+
+create table if not exists tiers (
+    id bigserial primary key,
+    code varchar not null,
+    name varchar not null,
+
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp
+);
+
+create unique index if not exists idx_tiers_code on tiers (code);
+
+create table if not exists employees (
+    id bigserial primary key,
+    "no" bigint not null,
+    name varchar not null,
+    tier_code varchar default null,
+    department_code varchar default null,
+    location_code varchar default null,
+    supervisor_no int default null,
+    salary bigint not null,
+    entry_date timestamptz not null,
+
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp,
+
+    constraint fk_employees_tier_code foreign key (tier_code) references tiers (code) on update cascade on delete set null,
+    constraint fk_employees_department_code foreign key (department_code) references departments (code) on update cascade on delete set null,
+    constraint fk_employees_location_code foreign key (location_code) references locations (code) on update cascade on delete set null
+);
+
+create unique index if not exists idx_employees_no on employees (no);
+
+alter table employees
+add constraint fk_employees_supervisor_no foreign key (supervisor_no) references employees ("no") on update cascade on delete set null;
